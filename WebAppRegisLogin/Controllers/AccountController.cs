5956c70453c41181302fb1e3ca673a7ca212e682
haolun <1204926758@qq.com>
2019-10-30 13:48:07 -0400

    Add project files.

---
+++ WebAppRegisLogin/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppRegisLogin.DBModel;
+using WebAppRegisLogin.Models;
+
+namespace WebAppRegisLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserDBEntities objuserDBEntities = new UserDBEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            UserModel objUserModel = new UserModel();
+            return View(objUserModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserModel objUserModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User objUser = new DBModel.User();
+                objUser.UserID = objUserModel.UserID;
+                objUser.UserName = objUserModel.UserName;
+                objUser.UserEmail = objUserModel.UserEmail;
+                objUser.UserImage = objUserModel.UserImage;
+                objUser.UserPassword = objUserModel.UserPassword;
+                objuserDBEntities.Users.Add(objUser);
+                objuserDBEntities.SaveChanges();
+                // new add
+                objUserModel = new UserModel();
+                objUserModel.SuccessMessage = "User is sucessfully added";
+                return View("Register");
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            LoginModel objLoginModel = new LoginModel();
+            return View(objLoginModel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel objLoginModel)
+        {
+            if(ModelState.IsValid)
+            {
+                if(objuserDBEntities.Users.Where(m => m.UserEmail == objLoginModel.Email && m.UserPassword ==
+                objLoginModel.Password).FirstOrDefault() == null)
+                {
+                    ModelState.AddModelError("Error", "Invalid Email and Password");
+                    return View();
+                }
+                else
+                {
+                    Session["Email"] = objLoginModel.Email;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return View("Login");
+        }
+
+
+    }
+}
---
+++ WebAppRegisLogin/Models/UserModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppRegisLogin.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "UserID is required")]
+        [Display(Name = "UserID:")]
+        public int UserID { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "UserName is required")]
+        [Display(Name = "UserName: ")]
+        public String UserName { get; set; }
+
+        [Display(Name = "UserEmail: ")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "UserEmail is required")]
+        public String UserEmail { get; set; }
+
+        [Display(Name = "UserImage: ")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "UserImage is required")]
+        public String UserImage { get; set; }
+
+        [Display(Name = "UserPassword: ")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "UserPassword is required")]
+        [DataType(DataType.Password)]
+        public String UserPassword { get; set; }
+
+        public String SuccessMessage { get; set; }
+
+    }
+
+}
